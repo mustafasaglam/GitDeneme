96c1f1c1dc312e001817e7f1674f29031d2073b0
Ms <Ms@MS>
2016-12-29 19:05:22 +0300

    test

---
+++ GitDeneme/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitDeneme.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            ViewBag.title = "TEST";
+            return View();
+        }
+    }
+}
